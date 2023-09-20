841161028a64d64b29bbde7c9d7091b9fb76764b
lazpetr <lazpetr@gmail.com>
2023-09-20 11:09:40 +0300

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+$app = include (__DIR__ . '/../src/App/bootstrap.php' );
+
+// phpinfo();
+
+// echo 'Laz The Magnificent! ' .  __DIR__;
+
+
+$app -> run();
---
+++ src/App/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+// include __DIR__ . '/../Framework/App.php';
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Framework\App;
+
+$app = new App();
+
+// $app -> run(); 
+return $app;
